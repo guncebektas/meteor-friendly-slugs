c2ff70894f7be464daa8ef109bc38c3d38df3006
gunce <guncebektas@gmail.com>
2023-11-04 16:09:21 +0300

    add: git

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'Generate URL friendly slugs from a field with auto-incrementation to ensure unique URLs.',
   // URL to the Git repository containing the source code for this package.
-  // git: 'https://github.com/guncebektas/meteor-friendly-slugs',
+  git: 'https://github.com/guncebektas/meteor-friendly-slugs',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
